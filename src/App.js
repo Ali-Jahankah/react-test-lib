0a3858b0f8845c80bbb3e251e0a566ba159e24a1
Ali Jahankah <aral2@yahoo.com>
2023-01-27 09:32:21 +0000

    fix the test

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ import './App.css';
 function App() {
   const[bg,setBg]=useState("black");
   const[color,setColor]=useState("white")
+  const[btnCondition,setBtnCondition]=useState(true)
   return (
     <div className="App">
-     <button className='change-color-btn' style={{background:bg,color:color}} onClick={()=>{setBg(bg==="gray"?"black":"gray");setColor(color==="black"?"white":"black")}} >{`Change BG to ${bg}`}</button>
+        <button className='change-color-btn'
+        style={{backgroundColor:bg,color:color}} 
+        onClick={()=>{setBg(bg==="gray"?"black":"gray");
+        setColor(color==="black"?"white":"black")}}
+        disabled={btnCondition} >
+        {`Change BG to ${bg==="gray"?"black":"gray"}`}
+        </button>
+      <br />
+      <input type="checkbox" name="checkbox" id="checking" defaultChecked={!btnCondition} onChange={(e)=>setBtnCondition(!btnCondition)} />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,27 @@ import App from './App';
 
 test("Button's color and BG change'", () => {
  render(<App></App>);
- const myButton=screen.getByRole("button",{name:"Change BG to black"});
+ const myButton=screen.getByRole("button",{name:"Change BG to gray"});
  expect(myButton).toHaveStyle({backgroundColor: "black"});
- expect(myButton).toHaveTextContent("Change BG to black");
-fireEvent.click(myButton);
-expect(myButton).toHaveStyle({backgroundColor: "gray"});
-expect(myButton).toHaveTextContent("Change BG to gray");
+ expect(myButton).toHaveTextContent("Change BG to gray");
+ 
+  
+ 
+  
+
 });
+test("Check box and button enabled and disabled",()=>{
+  render(<App></App>)
+  const checkbox=screen.getByRole("checkbox");
+  const myButton=screen.getByRole("button",{name:"Change BG to gray"})
+  expect(checkbox).not.toBeChecked();
+  expect(myButton).toBeDisabled();
+  fireEvent.click(checkbox);
+  expect(checkbox).toBeChecked();
+  expect(myButton).toBeEnabled();
+  fireEvent.click(myButton);
+
+expect(myButton).toHaveStyle({backgroundColor: "gray"});
+expect(myButton).toHaveTextContent("Change BG to black");
+  
+})
