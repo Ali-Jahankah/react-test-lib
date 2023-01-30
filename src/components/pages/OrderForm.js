01d63b39df1868cebcdbf852d8559ab842f75bb9
Ali Jahankah <aral2@yahoo.com>
2023-01-30 17:30:39 +0000

    fixing tests

--- src/components/pages/OrderForm.js
+++ src/components/pages/OrderForm.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 const OrderForm = () => {
   const submitForm = () => {};
   const [disabled, setDisabled] = useState(true);
+  const [span, setSpan] = useState("");
+
   return (
     <section className="order-form-section">
       <form onSubmit={submitForm}></form>
@@ -14,9 +16,15 @@ const OrderForm = () => {
           defaultChecked={!disabled}
           onChange={() => setDisabled(!disabled)}
         />
-        <label htmlFor="check-order">
+        <label
+          htmlFor="check-order"
+          className="terms-label"
+          onMouseOver={() => setSpan("This is a test")}
+          onMouseOut={() => setSpan("")}
+        >
           I agree to <a href="#">Terms and Conditions.</a>
         </label>
+        {span.length > 3 ? <span className="test-span">{span}</span> : ""}
       </div>
 
       <button type="submit" disabled={disabled}>
--- src/components/pages/test/OrderForm.test.js
+++ src/components/pages/test/OrderForm.test.js
@@ -1,13 +1,29 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import OrderForm from "../OrderForm";
-test("Check box unchecked firest | button disabled first | change them by clicking", () => {
+import userEvent from "@testing-library/user-event";
+test("Check box unchecked firest | button disabled first | change them by clicking", async () => {
+  const user = userEvent.setup();
   render(<OrderForm></OrderForm>);
   const checkBox = screen.getByLabelText("I agree to Terms and Conditions.");
   const btn = screen.getByRole("button", { name: "Confirm Order" });
 
   expect(checkBox).not.toBeChecked();
   expect(btn).toBeDisabled();
-  fireEvent.click(checkBox);
+  await user.click(checkBox);
   expect(checkBox).toBeChecked();
   expect(btn).not.toBeDisabled();
+});
+test("check popover on the page by hovering", async () => {
+  const user = userEvent.setup();
+  render(<OrderForm></OrderForm>);
+  const nullPop = screen.queryByText(/This is a test/i);
+  expect(nullPop).not.toBeInTheDocument();
+  const terms = screen.getByText(/Terms and Conditions./i);
+
+  await user.hover(terms);
+
+  const pop = screen.getByText(/This is a test/i);
+  expect(pop).toBeInTheDocument();
+  await user.unhover(terms);
+  expect(pop).not.toBeInTheDocument();
 });
